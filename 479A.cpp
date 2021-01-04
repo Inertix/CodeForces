dcb507158eb7e4f1bb4b7e64725c3506a4ee8435
d4r_ <76253938+darrancebeh@users.noreply.github.com>
2021-01-05 06:15:24 +0800

    Accepted.

---
+++ 479A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+
+    cin >> a >> b >> c;
+
+    int ans(0);
+    ans = max(ans, a+b+c);
+    ans = max(ans,(a*b*c));
+	ans = max(ans,(a+b)*c);
+	ans = max(ans,a*(b+c));
+	ans = max(ans,a+(b*c));
+	ans = max(ans,(a*b)+c);
+
+    cout << ans << endl;
+    return 0;
+}
